8be611581d23896ff9cadc5cc86abe7424ea7da3
Harvey <harveyjwilliams@aol.co.uk>
2020-02-02 00:07:07 +0100

    It parses just XML.

--- node/lib/fml.js
+++ node/lib/fml.js
@@ -1,9 +1,48 @@
+const xmlParser = new require('xml2js').Parser({
+    explicitArray: false // Otherwise children become arrays.
+})
+
 class FusionMarkupLanguage {
     constructor() {
 
     }
     parse(text) {
-        return JSON.parse(text);
+        try
+        {
+            const result = JSON.parse(text)
+
+            // console.log(result)
+
+            return result
+        }
+        catch (e)
+        {
+            try
+            {
+                let data = null
+                let error = null
+
+                xmlParser.parseString(text, (fail, result) => {
+                    error = fail
+                    data = result
+                })
+
+                if (error)
+                    throw error
+
+                return data
+
+                // return xmlParser.parseString(text, (err, result) => {
+                //     return result
+                // })
+            }
+            catch (e)
+            {
+                throw e;
+                // throw "Failed to parse."
+            }
+        }
+
     }
 }
 
--- node/test/test.js
+++ node/test/test.js
@@ -1,28 +1,55 @@
-var assert = require('assert');
+var assert = require('assert')
 
-const FusionMarkupLanguage = require('../index.js');
-const fml = new FusionMarkupLanguage();
+const FusionMarkupLanguage = require('../index.js')
+const fml = new FusionMarkupLanguage()
 
 describe('FusionMarkupLanguage', () => {
     describe('#parse()', () => {
-        it('should parse JSON', () => {
+        it('should parse just JSON', () => {
             const parsed = fml.parse(`
                 {
                     "person": {
                         "name": {
-                            "firstName": "Harvey"
+                            "firstName": "Rick",
+                            "lastName": "Astley"
                         }
                     }
                 }
-            `);
+            `)
 
             assert.deepEqual(parsed, {
                 person: {
                     name: {
-                        firstName: "Harvey"
+                        firstName: "Rick",
+                        lastName: "Astley"
                     }
                 }
             })
-        });
-    });
-});
+        })
+    })
+
+    describe('#parse()', () => {
+        it('should parse just XML', () => {
+            const parsed = fml.parse(`
+                <?xml version="1.0" encoding="UTF-8" ?>
+                <person>
+                    <name>
+                        <firstName>Rick</firstName>
+                        <lastName>Astley</lastName>
+                    </name>
+                </person>
+            `)
+
+            console.warn('parsed', parsed)
+
+            assert.deepEqual(parsed, {
+                person: {
+                    name: {
+                        firstName: "Rick",
+                        lastName: "Astley"
+                    }
+                }
+            })
+        })
+    })
+})
